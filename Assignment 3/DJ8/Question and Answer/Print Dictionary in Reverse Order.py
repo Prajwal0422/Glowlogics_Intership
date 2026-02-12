df05ea0f3e2db5644feb2128bc0c5279979c86d2
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-12 13:39:31 +0530

    DJ8 questions program and there solutions

---
+++ Assignment 3/DJ8/Question and Answer/Print Dictionary in Reverse Order.py
@@ -0,0 +1,4 @@
+s1 = {1:'tally',2:'oracle',3:'java',4:'python'}
+
+for k in reversed(s1):
+    print(k, ":", s1[k])
---
+++ Assignment 3/DJ8/Question and Answer/Print Dictionary in Reverse Orderanother.py
@@ -0,0 +1,3 @@
+s1 = {1:'tally',2:'oracle',3:'java',4:'python'}
+for k,v in reversed(s1.items()):
+    print(k,":",v)
