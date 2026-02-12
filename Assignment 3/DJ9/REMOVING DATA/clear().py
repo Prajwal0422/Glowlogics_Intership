f909dd02254a6de3149dbd4e0ca5378826e240d4
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-12 13:59:36 +0530

    DJ9 removing data program and there solutions

---
+++ Assignment 3/DJ9/REMOVING DATA/clear().py
@@ -0,0 +1,4 @@
+s1 = {"tally","oracle","java","python","vba","autocad"}
+
+s1.clear()
+print(s1)
---
+++ Assignment 3/DJ9/REMOVING DATA/remove().py
@@ -0,0 +1,4 @@
+s1 = {"tally","oracle","java","python","vba","autocad"}
+
+s1.remove("java")
+print(s1)
