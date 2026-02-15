aa2925fc695d7b401d63144c8a368424b9a4815b
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-15 15:45:48 +0530

    Day 12_02 is complete with all teached problems in the online session

---
+++ Day 12_02/Day 12_02.py
@@ -0,0 +1,69 @@
+ # UDF
+#
+# UDF+Parameters
+#
+# a=100
+# b=100
+#
+# Built in functions are:
+# print(),count(),len(),reverse()
+
+# USer defined functions aree:
+def add():
+     x=100
+     y=150
+     z=x+y
+     print(z)
+     print(x+y)
+
+def substract():
+     x = 100
+     y = 150
+     z = x - y
+     print(z)
+     print(x + y)
+def multiply():
+     x = 100
+     y = 150
+     z = x + y
+     print(z)
+     print(x + y)
+
+
+def division():
+     x = 100
+     y = 150
+     z = x - y
+     print(z)
+     print(x + y)
+
+add()
+substract()
+multiply()
+division()
+
+# Switch case as
+
+if(opt=1):
+    add()
+
+# UDF - paramneter
+# def(students(name:) - function with onE parameters
+# print (nAME)
+
+def student(name,age):
+print(name)
+print(age)
+print(marks)
+student('vikas',56,97)
+
+def addd(x,y):
+return x+y
+
+def sttuu(name,cource,collname="cmt"):
+return name, course,collname
+s1=student("vikaas",mba)
+print (1)
+
+
+si1=sttuu("poo",cbom",lsk)
