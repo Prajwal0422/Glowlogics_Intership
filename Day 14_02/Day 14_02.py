5af7ad9ce09acdddfcd8d242317c94473ed3fdc9
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-15 18:29:14 +0530

    Day 13_02 is complete with all teached problems in the online session

---
+++ Day 14_02/Day 14_02.py
@@ -0,0 +1,21 @@
+class calculate:
+    s=int(input("enter a number"))
+    t=int(input("enter a number"))
+    c=0
+
+    def add(self.me)
+        print(me.a)
+        print(me.b)
+        me.c=me.a+me.b
+
+    def sub(me):
+        print(me.a)
+        print(me.b)
+        me.c = me.a + me.b
+
+    def mult(me):
+        print(me.a)
+        print(me.b)
+        me.c = me.a + me.b
+
+obj=calculate()
