07fab08388065b9109777158b3c2883a123a9e7e
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-19 16:03:34 +0530

    new.py

---
+++ Day 11_02/new.py
@@ -0,0 +1,15 @@
+class CMR:
+    def __init__(self, name, age, course):
+        print("CMR:", name, age, course)
+
+class PES:
+    def __init__(self, name, age, course):
+        print("PES:", name, age, course)
+
+print("CMR Students")
+CMR("Sanjay",19,"MBA")
+CMR("Pooja",22,"MCA")
+
+print("PES Students")
+PES("Raghu",21,"BE")
+PES("Sham",22,"MTech")
