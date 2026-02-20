a9f459e1138c39cd8dd8ade024bb14b9f4963784
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-20 16:40:16 +0530

    Day 17_02 date is updated all solve problems

---
+++ Day 17_02/Day 17_02.py
@@ -0,0 +1,30 @@
+
+import numpy as np
+arr=np.array([[1,2,3,4],[5,6,7,8]])
+print(arr)
+
+print("iem",arr.dtype)
+
+print("size",arr.size)
+
+print("shape",arr.shape)
+
+#print("reshape",arr.reshape(2,3))
+
+print("max",arr.max())
+print("min",arr.min())
+
+print("sum",arr.sum())
+#print("square",arr.sqrt())
+
+q=np.array([1,2,3,4,5,6,7,8])
+w=np.array([1,2,3,4,5,6,7,8])
+# print(np.vstack(q,w),
+# np.hstack(q,w))
+
+print(np.multiply(123,3))
+print(np.multiply("123\t",3))
+
+
+
+
---
+++ Day 17_02/Readme.md
@@ -0,0 +1,15 @@
+Array concatination
+
+with certical stacking and horizontal stacking whiuch allows 2 or more to add each other
+
+np.vstack(a,b)  used to vertical stack
+np.hstack(a,b) used for horizontal stack
+
+numpy array
+proveds a way to create an array
+
+numercial string functions/
+
+np.char.add = to add the strings to the array
+
+numpy.char.multiply()= used to multily the data
