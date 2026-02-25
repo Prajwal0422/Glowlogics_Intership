1b81aa8dffa6149de0302db186cb399b4b3923e1
Prajwal Y Jain <prajuyjain2204as@gmail.com>
2026-02-25 17:58:03 +0530

    Day 23_02 is completed class and submitted al class explain solution

---
+++ Day 23_02/Day 23_02.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+asdf = np.array([3,8,1,10])
+
+plt.plot(asdf, marker = 'o')
+plt.show()
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# asdf = np. array([3,8,1,10])
+#
+# plt.plot(asdf, marker = 'o')
+# plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+asdf = np.array([3,8,1,10])
+
+plt.plot(asdf, marker = 'o:*')
+plt.show()
---
+++ Day 23_02/Readme.md
@@ -0,0 +1,21 @@
+C:>pip install matplotlib------>PL
+numpy + pandas + graph
+
+C:>pip install numpy
+
+C:>pip install pandas
+
+c:>pip install matplotlib
+
+Marker   Description
+'0'
+'*'
+'.'
+'/'
+
+
+ms is a marker size
+me marker edge colour 
+mfc is marker face colour
+'D' this is marker which gves diamond shape
+linewidth gives width of the linbe
